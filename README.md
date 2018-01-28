efc1d45213f1a69999d03ef515936a074e6c2f56
ivankmk <kumeyko@hotmail.com>
2018-01-28 16:46:50 +0100

    script created

--- README.md
+++ README.md
@@ -1,6 +1,44 @@
 # Night Owls Detector
 
-[TODO. There will be project description]
+Python script revert night persons who at least once submitted the Devman exercise during the night (from 00:00 till 05:59).
+
+
+# Requirements
+
+ - Python 3.5
+ - Packages: requests==2.11.1, pytz==2016.7
+
+ # How to launch
+
+ ```bash
+  $ python seek_dev_nighters.py # possibly requires call of python3 executive instead of just python
+  ------------------------------------------------------------
+  There is the list of midnighters:
+
+  elena_barnz
+  ld38475474
+  id93321377
+  bondarenko.vs
+  RomanGagarin
+  teltmr
+  maya_pavlova_kubarik
+  id44804514
+  kserg4356
+  karandasov
+  PavelShturm
+  anatolijplotnikov1
+  se.mrzv
+  rusrussel777
+  dina.draguzya
+  apaticmail
+  tyulalex
+  IvanKumeyko
+  qqyatut
+  inoobasik
+  id357826325
+  inkpie
+  ------------------------------------------------------------
+ ```
 
 # Project Goals
 
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,38 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import requests
+import pytz
+import json
+from datetime import datetime
 
-def get_midnighters():
-    pass
+
+def get_all_submissions():
+    all_submissions = []
+    url = 'https://devman.org/api/challenges/solution_attempts/'
+    total_pages = requests.get(url,params={'page':1}).json()['number_of_pages']
+    for page in range(1, total_pages+1):
+        each_page = requests.get(url,params={'page':page}).json()['records']
+        all_submissions.extend(each_page)
+    return all_submissions
+
+def get_midnighters(all_devman_submissions):
+    midnighters = set()
+    night_begin = datetime.strptime('00:00', '%H:%M').time()
+    night_end = datetime.strptime('05:59', '%H:%M').time()
+    for submission in all_devman_submissions:
+        timezone = pytz.timezone(submission['timezone'])
+        submission_time = datetime.fromtimestamp(submission['timestamp'])
+        local_time = pytz.utc.localize(
+                submission_time
+                ).astimezone(timezone).time()
+        if  night_end > local_time > night_begin:
+            midnighters.add(submission['username'])
+    return midnighters
+
 
 if __name__ == '__main__':
-  pass
+    delimiter = '-'*60
+    all_devman_submissions = get_all_submissions()
+    print(delimiter)
+    print('There is the list of midnighters:\n')
+    for midnighter in get_midnighters(all_devman_submissions):
+        print(midnighter)
+    print(delimiter)
