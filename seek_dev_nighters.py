e421d8dc32785c7b0d3ca2605850681a4bf18e9c
ivankmk <kumeyko@hotmail.com>
2018-02-04 22:13:57 +0100

    implemented yeld approach

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -4,37 +4,42 @@ import json
 from datetime import datetime
 
 
-def get_all_submissions():
-    all_submissions = []
+def get_all_attempts():
     url = 'https://devman.org/api/challenges/solution_attempts/'
     total_pages = requests.get(url,
                                params={'page': 1}).json()['number_of_pages']
     for page in range(1, total_pages+1):
-        each_page = requests.get(url, params={'page': page}).json()['records']
-        all_submissions.extend(each_page)
-    return all_submissions
+        yield requests.get(url, params={'page': page}).json()['records']
 
 
 def get_midnighters(all_devman_submissions):
     midnighters = set()
-    night_begin = datetime.strptime('00:00', '%H:%M').time()
-    night_end = datetime.strptime('05:59', '%H:%M').time()
+    night_begin = 0
+    night_end = 6
+
     for submission in all_devman_submissions:
         timezone = pytz.timezone(submission['timezone'])
         submission_time = datetime.fromtimestamp(submission['timestamp'])
         local_time = pytz.utc.localize(
                 submission_time
-                ).astimezone(timezone).time()
+                ).astimezone(timezone).time().hour
         if night_end > local_time > night_begin:
             midnighters.add(submission['username'])
     return midnighters
 
 
+def get_unique_midnighters(all_submission):
+    unique_midnighters = set()
+    for a in get_all_attempts():
+        for aa in get_midnighters(a):
+            unique_midnighters.add(aa)
+    return unique_midnighters
+
 if __name__ == '__main__':
     delimiter = '-'*60
-    all_devman_submissions = get_all_submissions()
+    all_submission = get_all_attempts()
     print(delimiter)
     print('There is the list of midnighters:\n')
-    for midnighter in get_midnighters(all_devman_submissions):
+    for midnighter in get_unique_midnighters(all_submission):
         print(midnighter)
     print(delimiter)
